02ca5c492e2b4dc85c4f057f25792377ff4a9d7b
Wilson Qin <wilson.qin@gmail.com>
2019-01-29 18:06:37 -0500

    test.c change assert expected expressions to match the hash_table.c spec cmts. make num_results a caller local var.

--- test.c
+++ test.c
@@ -14,7 +14,7 @@ int main(void) {
 
   hashtable* ht=NULL;
   int num_tests = 20;
-  assert(allocate(&ht, num_tests));
+  assert(allocate(&ht, num_tests) == 0);
 
   int seed = 1;
   srand(seed);
@@ -26,18 +26,19 @@ int main(void) {
   for (int i = 0; i < num_tests; i += 1) {
     keys[i] = rand();
     values[i] = rand();
-    assert(put(ht, keys[i], values[i]));
+    assert(put(ht, keys[i], values[i]) == 0);
     printf("\t(%d -> %d) \n", keys[i], values[i]);
   }
 
   int num_values = 1;
   int results[num_values];
-  int* num_results = NULL;
+  int num_results;
 
   for (int i = 0; i < num_tests; i += 1) {
     int index = rand() % num_tests;
     keyType target_key = keys[index];
-    assert(get(ht, target_key, results, num_values, num_results));
+    num_results = 0;
+    assert(get(ht, target_key, results, num_values, &num_results) == 0);
     if (results[0] != values[index]) {
       printf("Test failed with key %d. Got value %d. Expected value %d.\n", target_key, results[0], values[index]);
       return 1;
@@ -49,14 +50,15 @@ int main(void) {
 
   for (int i = 0; i < num_tests; i += 1) {
     keyType target_key = keys[i];
-    assert(erase(ht, target_key));
-    assert(get(ht, target_key, results, num_values, num_results));
-    if ((*num_results) != 0) {
-      printf("Test failed with key %d. Expected it to be erased, but got %d matches.\n", target_key, *num_results);
+    num_results = 0;
+    assert(erase(ht, target_key) == 0);
+    assert(get(ht, target_key, results, num_values, &num_results) == 0);
+    if ( num_results != 0) {
+      printf("Test failed with key %d. Expected it to be erased, but got %d matches.\n", target_key, num_results);
       return 1;
     } 
   }
-  assert(deallocate(ht));
+  assert(deallocate(ht) == 0);
   printf("Passed tests for erasing.\n");
   printf("All tests have been successfully passed.\n");
   return 0;
